2c3b4fb64e48ff1500bf2349d6895236c2f85fdc
Mrinmay Das <mrinmaydas2604@gmail.com>
2023-03-10 17:07:17 +0000

    New file
    It can sense distance and makes a buzzing noise based on distance.

---
+++ TRYY.ino
@@ -0,0 +1,31 @@
+// C++ code
+//
+const int TrigPin=9;
+const int EchoPin=10;
+const int ledPin=8;
+long dur;
+int dist;
+void setup()
+{
+  pinMode(TrigPin, OUTPUT);
+  pinMode(EchoPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  digitalWrite(TrigPin,LOW);
+  delay(2); // Wait for 1000 millisecond(s)
+  digitalWrite(TrigPin, HIGH);
+  delay(10); // Wait for 1000 millisecond(s)
+  digitalWrite(TrigPin,LOW);
+  dur = pulseIn(EchoPin, HIGH);
+  dist = dur * 0.034 / 2;
+  Serial.print(dist);
+  Serial.println("cm");
+  if(dist>65)
+    digitalWrite(ledPin,HIGH);
+
+  else
+    digitalWrite(ledPin,LOW);
+}
